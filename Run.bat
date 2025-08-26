0652f71b36e96619a5c227943744965572bb066c
ervy <84644035+ervum@users.noreply.github.com>
2025-08-26 09:40:30 -0300

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,8 +1,7 @@
 @echo off
 
-REM Runs the python script and passes all command-line arguments to it.
+REM Runs the Python script and passes all command-line arguments to it. In short, if this Batch file was ran from Terminal as `Run.bat <Command> --<Arguments>`, it'd be the same as running the script in said way. In lieu, if it's just executed normally, nothing will be passed, and the defaults will take over.
 python Silicon.py %*
 
 REM Keeps the window open to see the output.
-
 pause
